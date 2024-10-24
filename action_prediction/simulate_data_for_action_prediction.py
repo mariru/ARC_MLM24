59049e0ed1a0e89973bcd61491ee548e459d32d5
mariru <1409833+mariru@users.noreply.github.com>
2024-10-24 17:03:12 -0500

    encode action embeddings

--- action_prediction/simulate_data_for_action_prediction.py
+++ action_prediction/simulate_data_for_action_prediction.py
@@ -1,14 +1,24 @@
 import torch
 from torch.utils.data import Dataset
 import arcle
+import pickle
 import gymnasium as gym
 import time
 import numpy as np
 from arcle.loaders import MiniARCLoader
 from arcle.wrappers import BBoxWrapper
+#from transformers import BartTokenizer, BartModel
+
+#BartTokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+#Bart = BartModel.from_pretrained('facebook/bart-base')
 
 from arcle.envs.o2arcenv import O2ARCv2Env
 operations = O2ARCv2Env().operations
+
+action_descriptions = pickle.load(open('generated_action_descriptions.pkl', 'rb'))
+
+def get_action_embedding(action):
+    return torch.tensor(action_descriptions[action]['embedding'])
 
 def get_action_name(action):
     idx = int(action['operation'])
@@ -19,9 +29,9 @@ def get_action_name(action):
 
 class GymDataset(Dataset):
     def __init__(self, num_samples, max_grid_size=(5, 5)):
-        env = gym.make('ARCLE/RawARCEnv-v0',render_mode='ansi',data_loader = MiniARCLoader(),max_grid_size=max_grid_size)
-        self.env = BBoxWrapper(env)
-        obs, info = env.reset()
+        self.env = gym.make('ARCLE/RawARCEnv-v0',render_mode='ansi',data_loader = MiniARCLoader(),max_grid_size=max_grid_size)
+        #self.env = BBoxWrapper(self.env)
+        obs, info = self.env.reset()
         self.num_samples = num_samples
         self.data = []
         self.generate_data()
@@ -41,14 +51,19 @@ class GymDataset(Dataset):
 
             # Store the data: (prev_state, new_state, action)
             # TODO: uncomment the following line
-            # action = self.encode_arcle_action(action)
+            action = self.encode_arcle_action(action)
             self.data.append((prev_obs, obs, action))
 
     def encode_arcle_action(self, action):
         # TODO: Map the action to text
         action_name = get_action_name(action)
+        action_embedding = get_action_embedding(action_name)
+        #inputs = BartTokenizer(action_name, return_tensors="pt")
+        #outputs = Bart(**inputs)
+
+        #return outputs.last_hidden_state
         # TODO: Encode the text using the text encoder
-        return torch.tensor(np.random.rand(128))
+        return action_embedding
 
     def __len__(self):
         return len(self.data)
